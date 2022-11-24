3b7b85caa7a72f2e443267020506779e04496e5f
Massimo Biagioli <biagiolimassimo@gmail.com>
2022-11-24 17:39:53 +0100

    Add dotenv

---
+++ src/config/index.ts
@@ -0,0 +1,13 @@
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const DEFAULT_SERVER_PORT = 3000;
+
+export interface Settings {
+    serverPort: number;
+}
+
+export const settings: Settings = {
+    serverPort: process.env.SERVER_PORT ? Number(process.env.SERVER_PORT) : DEFAULT_SERVER_PORT,
+}
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import createApp from './app';
+import {settings} from "./config";
 
 const app = createApp();
 
 const start = async () => {
     try {
-        await app.listen({ port: 3000 });
+        await app.listen({port: settings.serverPort});
         app.log.info('Server started successfully');
     } catch (err) {
         app.log.error(err);
