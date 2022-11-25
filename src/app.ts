062302359c4749543a180bc0a4e1e85733d87465
Massimo Biagioli <biagiolimassimo@gmail.com>
2022-11-25 09:32:26 +0100

    add mongo uri to env var

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,14 @@
 import { fastify } from 'fastify'
 import pino from 'pino'
 import db from './model'
+import { settings } from './config'
 
 const createApp = () => {
   const app = fastify({
     logger: pino({ level: 'info' }),
   })
 
-  app.register(db, { uri: 'mongodb://localhost:27017/device-manager' })
+  app.register(db, { uri: settings.mongoUri })
 
   return app
 }
--- src/config/index.ts
+++ src/config/index.ts
@@ -6,10 +6,13 @@ const DEFAULT_SERVER_PORT = 3000
 
 export interface Settings {
   serverPort: number
+  mongoUri: string
 }
 
 export const settings: Settings = {
   serverPort: process.env.SERVER_PORT
     ? Number(process.env.SERVER_PORT)
     : DEFAULT_SERVER_PORT,
+
+  mongoUri: process.env.MONGO_URI || '',
 }
