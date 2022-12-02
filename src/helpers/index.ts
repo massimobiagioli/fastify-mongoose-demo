7aede50f4648ec3461f7a1ce5d56243b0372c816
Massimo Biagioli <biagiolimassimo@gmail.com>
2022-12-02 16:57:41 +0100

    Add test for error create new device

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,51 +1,53 @@
-import {fastify, FastifyPluginAsync} from "fastify";
-import autoload from "@fastify/autoload";
-import path from "path";
-import {DeviceRoutesPlugin} from "../routes/device";
+import { fastify, FastifyPluginAsync } from 'fastify'
+import autoload from '@fastify/autoload'
+import path from 'path'
+import { DeviceRoutesPlugin } from '../routes/device'
 
 export type CreateTestAppOptions = {
-    autoLoadPlugins?: boolean
-    autoLoadRoutes?: boolean
-    decorates?: Record<string, unknown>
-    routes?: FastifyPluginAsync[]
+  autoLoadPlugins?: boolean
+  autoLoadRoutes?: boolean
+  decorates?: Record<string, unknown>
+  routes?: FastifyPluginAsync[]
 }
 
 const defaultCreateTestAppOptions: CreateTestAppOptions = {
-    autoLoadPlugins: true,
-    autoLoadRoutes: true,
-    decorates: {},
-    routes: [],
+  autoLoadPlugins: true,
+  autoLoadRoutes: true,
+  decorates: {},
+  routes: [],
 }
 
-export const createTestApp = (createTestAppOptions: CreateTestAppOptions = defaultCreateTestAppOptions) => {
-    const app = fastify()
+export const createTestApp = (
+  createTestAppOptions: CreateTestAppOptions = defaultCreateTestAppOptions,
+) => {
+  const app = fastify()
 
-    const options = {
-        ...defaultCreateTestAppOptions,
-        ...createTestAppOptions
-    }
+  const options = {
+    ...defaultCreateTestAppOptions,
+    ...createTestAppOptions,
+  }
 
-    if (options?.autoLoadPlugins) {
-        app.register(autoload, {
-            dir: path.join(__dirname, '../plugins'),
-        })
-    }
+  if (options?.autoLoadPlugins) {
+    app.register(autoload, {
+      dir: path.join(__dirname, '../plugins'),
+    })
+  }
 
-    if (options?.decorates) {
-        Object.entries(options.decorates).forEach(([key, value]) => {
-            app.decorate(key, value)
-        })
-    }
+  if (options?.decorates) {
+    Object.entries(options.decorates).forEach(([key, value]) => {
+      app.decorate(key, value)
+    })
+  }
 
-    if (options?.autoLoadRoutes) {
-        app.register(autoload, {
-            dir: path.join(__dirname, '../routes'),
-        })
-    } else {
-        options?.routes?.forEach(route => {
-            app.register(route)
-        })
-    }
+  if (options?.autoLoadRoutes) {
+    app.register(autoload, {
+      dir: path.join(__dirname, '../routes'),
+    })
+  } else {
+    options?.routes?.forEach((route) => {
+      app.register(route)
+    })
+  }
 
-    return app
+  return app
 }
